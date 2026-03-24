628c54eade5e54daf466ea0e6deeb79ebd8ad3d6
MichalSamson2Pb <michalsamson29@gmail.com>
2026-03-24 14:08:05 +0100

    Update pracownik.py

--- pyton/pracownik.py
+++ pyton/pracownik.py
@@ -1,3 +1,5 @@
+import random
+
 class Pracownik:
     def __init__(self, id_p, imie_p, nazwisko_p, nr_zamowienia):
         self.id_p = id_p
@@ -6,11 +8,10 @@ class Pracownik:
         self.nr_zamowienia = nr_zamowienia
 
 class Klient:
-    def __init__(self, id_k, imie_k, nazwisko_k, telefon, nr_zamowienia):
+    def __init__(self, id_k, imie_k, nazwisko_k, nr_zamowienia):
         self.id_k = id_k
         self.imie_k = imie_k
         self.nazwisko_k = nazwisko_k
-        self.telefon = telefon
         self.nr_zamowienia = nr_zamowienia
 
 class Produkt:
@@ -43,3 +44,36 @@ m2 = Magazyn('2')
 Łukasz = Pracownik('1', 'Łukasz', 'Michalak', '1')
 Sławomir = Pracownik('2', 'Sławomir', 'Matuszewski', '2')
 Miłosz = Pracownik('3', 'Miłosz', 'Łabenda', '3')
+
+Szymon = Klient('1', 'Szymon', 'Walkowiak', '1')
+Franciszek = Klient('2', 'Franciszek', 'Musielak', '2')
+Jędrzej = Klient('3', 'Jędrzej', 'Szukowski', '3')
+
+produkty = [japko, gołomb, masło, agregat_prądotwórczy, woda_po_ogurkah]
+pracownicy = [Łukasz, Sławomir, Miłosz]
+klienci = [Szymon, Franciszek, Jędrzej]
+
+zamowienia = []
+
+for i in range(1, 101):
+    produkt = random.choice(produkty)
+    klient = random.choice(klienci)
+    pracownik = random.choice(pracownicy)
+
+    zam = Zamowienie(
+        nr_zamowienia=str(i),
+        koszt=produkt.cena,
+        id_k=klient.id_k,
+        id_p=pracownik.id_p,
+        nazwa=produkt.nazwa
+    )
+
+    zamowienia.append(zam)
+    
+    for z in zamowienia:
+        print("Zamówienie nr:", z.nr_zamowienia)
+        print("Produkt:", z.nazwa)
+        print("Koszt:", z.koszt)
+        print("ID klienta:", z.id_k)
+        print("ID pracownika:", z.id_p)
+        print("----------------------")
